6edc58f66663bc12d856a6e2e7fe724b1869b6e0
Norman Xu <Norman.Xu@.ef.com>
2010-09-20 09:52:08 +0800

    recommit

--- salespages.bat
+++ salespages.bat
@@ -1,1 +1,1 @@
-jsmake.pl -input d:/EF/FrontEnd/lib/ -output salespages.js -packages et.ui.autocomplete et.util.initVideoLinks et.util.initTourSlideshow jquery.sidest -compression closure
+jsmake.pl -input d:/EF/FrontEnd/lib/ -output salespages.js -packages jquery.sidest jquery.texttip et.ui.autocomplete et.ui.imageflow et.util.initVideoLinks et.util.initTourSlideshow -compression closure
