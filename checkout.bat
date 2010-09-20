866a2794258c051115e47aac1b8891c2b8b6a6d7
Norman Xu <Norman.Xu@.ef.com>
2010-09-20 10:09:11 +0800

    rename mvcclienthack

--- checkout.bat
+++ checkout.bat
@@ -1,1 +1,1 @@
-jsmake.pl -input d:/EF/FrontEnd/lib/ -output checkout.js -packages jquery.texttip jquery.dropTo -compression packer 
+jsmake.pl -input d:/EF/FrontEnd/lib/ -output checkout.js -packages jquery.texttip jquery.dropTo et.util.initMvcAdditionalEvents -compression closure 
