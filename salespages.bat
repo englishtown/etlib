9d9148c8816eebe70757fc341273bc1f974c319d
Norman Xu <Norman.Xu@.ef.com>
2010-09-20 09:54:10 +0800

    1. add imageflow 2. update salespage, include mvc hack

--- salespages.bat
+++ salespages.bat
@@ -1,1 +1,1 @@
-jsmake.pl -input d:/EF/FrontEnd/lib/ -output salespages.js -packages jquery.sidest jquery.texttip et.ui.autocomplete et.ui.imageflow et.util.initVideoLinks et.util.initTourSlideshow -compression closure
+jsmake.pl -input d:/EF/FrontEnd/lib/ -output salespages.js -packages jquery.sidest jquery.texttip et.ui.autocomplete et.ui.imageflow et.util.initVideoLinks et.util.initTourSlideshow et.util.initMvcClientHacks -compression closure
