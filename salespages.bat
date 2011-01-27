011b7d5a0ee077913e276acc16b41ad36a9b9a89
Norman.Xu <Norman.Xu@ef.com>
2011-01-28 01:33:40 +0800

    fix overlay issue on ipad , the stupid mobile safari developer totally confused about the concept of viewport and window, so position: fixed cannot work as expected, and leave a lot of bugs there that prevent the issue to be fixed. for example, onscroll only fires when scrolling is done. so what we did here is to make the masking layer cover the entire page so no matter where user scrolled to, he/she will see the overlay and then fix the lightbox to proper position relative to viewport
    this is tricky but is done by getBoundingClientRect of document.body

--- salespages.bat
+++ salespages.bat
@@ -1,1 +1,1 @@
-jsmake.pl -input d:/EF/FrontEnd/lib/ -output salespages.js -packages jquery.sidest jquery.texttip jquery.iecss3 jquery.pngfix et.ui.autocomplete et.ui.imageflow et.util.initVideoLinks et.util.initTourSlideshow et.util.initAlignmentHelper et.util.tagger  -compression closure
+jsmake.pl -input d:/EF/FrontEnd/lib/ -output salespages.js -packages jquery.support jquery.sidest jquery.texttip jquery.iecss3 jquery.pngfix et.ui.autocomplete et.ui.imageflow et.util.initVideoLinks et.util.initTourSlideshow et.util.initAlignmentHelper et.util.tagger 
