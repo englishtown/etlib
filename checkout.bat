671cf268a0beeec8070705a6fb6604ebdb71ae1b
normanzb <Norman.Xu@ef.com>
2011-04-18 11:07:33 +0800

    fix a bug in initVideoLinks, upgrade function.proto.bind to compatible with ecmascript 5

--- checkout.bat
+++ checkout.bat
@@ -1,1 +1,1 @@
-jsmake.pl -input d:/EF/FrontEnd/lib/ -output checkout.js -packages jquery.texttip jquery.dropTo jquery.sidest jquery.pngfix et.ui.autocomplete et.util.initTourSlideshow et.util.initMvcAdditionalEvents et.util.tagger et.util.sessionStore et.ui.scrollbar et.util.hoverfix et.util.sizeTagger
+jsmake.pl -input d:/EF/FrontEnd/lib/ -output checkout.js -packages jquery.texttip jquery.dropTo jquery.sidest jquery.pngfix et.ui.autocomplete et.util.initTourSlideshow et.util.initMvcAdditionalEvents et.util.sessionStore et.ui.scrollbar et.util.hoverfix et.util.sizeTagger
